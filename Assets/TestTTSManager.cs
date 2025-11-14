deae9a377ec1532fbb0d0e7f4a9af55982b640ae
Tim Shaw <70497517+TimShaw1@users.noreply.github.com>
2025-11-13 19:41:26 -0500

    add callback

--- Assets/TestTTSManager.cs
+++ Assets/TestTTSManager.cs
@@ -14,7 +14,12 @@ public class TestTTSManager : MonoBehaviour
         // Request an audio file and save it to Assets/helloWorld.mp3.
         // Note that the file extension is omitted. The extension is determined by the file format
         // set in the TTS config file.
-        ttsManager.GenerateSpeechFileFromText("Hello World!", "helloWorld", Application.dataPath);
+        ttsManager.GenerateSpeechFileFromText(
+            "Hello World!", 
+            "helloWorld", 
+            Application.dataPath,
+            path => Debug.Log("File created at: " + path)
+        );
 
         // Request an audio clip and play it through the TTS manager's audio source
         ttsManager.GenerateSpeechAudioClipFromText(
